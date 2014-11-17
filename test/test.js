650454132ffcd50c6ae311e817b09fc2d5ad3867
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2014-11-18 08:48:39 +0900

    add routes test

--- test/test.js
+++ test/test.js
@@ -6,15 +6,28 @@ require('should');
 
 describe('routes', function () {
     describe( 'indexのテスト' , function () {
-        it('should display index page with title', function (done) {
-        	var url = '/users';
-            req.get(url).end(function(err, ret) { // *
+        it('ログインページ', function (done) {
+        	var url = '/login';
+            req.get(url).end(function( err, ret) { // *
                 var res = ret.res,
                     statusCode = res.statusCode,
                     text = res.text;
  
                 statusCode.should.equal(200);
-                text.should.equal('respond with a resource');
+                text.should.containEql('ログイン');
+                done();
+            });
+        });
+        it('未ログインでホームにアクセス', function (done) {
+        	var url = '/';
+            req.get(url).end(function( err , ret) { // *
+                var res = ret.res,
+                    statusCode = res.statusCode;
+                    text = res.text;
+ 
+                statusCode.should.equal(303);
+                // res.redirect.should.equal('/regist');
+                text.should.containEql('新規登録');
                 done();
             });
         });
